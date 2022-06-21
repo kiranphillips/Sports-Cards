636f438f0a4c75b0c8e698555fd7da9156918b8d
Kiran Phillips <Kiranphillips61@gmail.com>
2022-06-21 11:21:04 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header'
+import Teams from './components/Teams'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Teams />
+    </>
+  );
+}
+
+export default App;
