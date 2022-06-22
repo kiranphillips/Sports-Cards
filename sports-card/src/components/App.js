36c32b342101e35e4e6a2ea41beec15050db106a
Shane Dorsey <scdorsey22@gmail.com>
2022-06-22 12:26:32 -0700

    updates

--- sports-card/src/components/App.js
+++ sports-card/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import './App.css';
 import TeamPage from './TeamPage';
+import PlayerPage from './PlayerPage';
 
 function App() {
 
   return (
     <div className="App">
       < TeamPage />
+      <PlayerPage />
     </div>
   );
 }
