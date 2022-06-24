6cf3b77144b7ac67c8a3bc1b23acfbea50f82b07
Shane Dorsey <scdorsey22@gmail.com>
2022-06-23 18:33:46 -0700

    styling header

--- sports-card/src/components/Header.js
+++ sports-card/src/components/Header.js
@@ -4,10 +4,11 @@ function Header () {
 
     return (
         <div id="nbaheading">
+            <img className="logo" alt="nba!" src="https://images.ctfassets.net/v3n26e09qg2r/3Q7dRCFKRz0J8mRZ8SIR0Z/78ff27249f6eef5aa51120f29d579432/nba_1200.png" />
             <h1 className="title1">
-                <i className="title2" ></i>
             NBA Teams
             </h1>
+            
     </div>
     )
 }
